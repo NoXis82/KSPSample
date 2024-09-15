b2aa7c832b65053187350e63f3c0ab8c871a5645
Tumanov Vycheslav <63115274+NoXis82@users.noreply.github.com>
2024-09-15 21:17:00 +0300

    add dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,17 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+pluginManagement {
+
+    val kotlinVersion: String by settings
+    val kspVersion: String by settings
+
+    plugins {
+        id("com.google.devtools.ksp") version kspVersion
+        kotlin("jvm") version kotlinVersion
+        id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+    }
+
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
 }
 rootProject.name = "KSPSample"
-
